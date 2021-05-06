1f31f08c23fd799e8409585e37f45a4e9a5464bb
Vlad-Stefan Harbuz <vlad@vladh.net>
2021-05-06 15:43:58 +0200

    Add description line parsing

--- httpdomain-to-openapi.py
+++ httpdomain-to-openapi.py
@@ -7,6 +7,44 @@ import sys
 import yaml
 
 
+def prepare_docstring(s, tabsize=4):
+    """
+    Convert a docstring into lines of parseable reST.  Remove common leading
+    indentation, where the indentation of a given number of lines (usually just
+    one) is ignored.
+
+    Return the docstring as a list of lines usable for inserting into a docutils
+    ViewList (used as argument of nested_parse().)  An empty line is added to
+    act as a separator between this docstring and following content.
+
+    NOTE: This function is from Sphinx.
+    """
+    # NOTE: Do we really want to ignore indentation of the first line?
+    ignore = 1
+    lines = s.expandtabs(tabsize).splitlines()
+    # Find minimum indentation of any non-blank lines after ignored lines.
+    margin = sys.maxsize
+    for line in lines[ignore:]:
+        content = len(line.lstrip())
+        if content:
+            indent = len(line) - content
+            margin = min(margin, indent)
+    # Remove indentation from ignored lines.
+    for i in range(ignore):
+        if i < len(lines):
+            lines[i] = lines[i].lstrip()
+    if margin < sys.maxsize:
+        for i in range(ignore, len(lines)):
+            lines[i] = lines[i][margin:]
+    # Remove any leading blank lines.
+    while lines and not lines[0]:
+        lines.pop(0)
+    # make sure there is an empty line at the end
+    if lines and lines[-1]:
+        lines.append('')
+    return lines
+
+
 def import_object(import_name):
     module_name, expr = import_name.split(':', 1)
     mod = __import__(module_name)
@@ -63,9 +101,36 @@ def print_usage_and_exit():
     sys.exit(1)
 
 
+def is_docstring_line_directive(docstring_line):
+    return docstring_line.startswith(':')
+
+
+def remove_start_and_end_empty_strings(lines):
+    if len(lines) == 0:
+        return lines
+    while len(lines[0]) == 0:
+        lines.pop(0)
+    while len(lines[-1]) == 0:
+        lines.pop(-1)
+    return lines
+
+
 def make_openapi_operation_object(view_docstring):
+    lines = prepare_docstring(view_docstring)
+    directive_lines = remove_start_and_end_empty_strings(
+        [line for line in lines if is_docstring_line_directive(line)]
+    )
+    non_directive_lines = remove_start_and_end_empty_strings(
+        [line for line in lines if not is_docstring_line_directive(line)]
+    )
+    summary_line = non_directive_lines[0]
+    description_lines = remove_start_and_end_empty_strings(non_directive_lines[1:])
+    print('\n'.join(directive_lines))
+    print('------------------------------------')
+
     return {
-        'da': 'da!',
+        'summary': summary_line,
+        'description': '\n'.join(description_lines),
     }
 
 
